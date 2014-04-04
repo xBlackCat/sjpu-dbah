56807fde11467a8ac5c13d56e13e1792ab842109
xBlackCat <xBlackCat@localhost>
2014-04-04 15:41:55 +0400

    Extract interfaces for create any type of builders. Just in case

--- src/java/org/xblackcat/sjpu/storage/impl/IAHBuilder.java
+++ src/java/org/xblackcat/sjpu/storage/impl/IAHBuilder.java
@@ -1,13 +1,11 @@
 package org.xblackcat.sjpu.storage.impl;
 
 import org.xblackcat.sjpu.storage.IAH;
-import org.xblackcat.sjpu.storage.StorageSetupException;
 
 /**
  * 21.02.13 11:46
  *
  * @author xBlackCat
  */
-interface IAHBuilder<P> {
-    <T extends IAH> T build(Class<T> target, P helper) throws StorageSetupException;
+interface IAHBuilder<P> extends IBuilder<P, IAH> {
 }
